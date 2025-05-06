87e3d32259cb72096deaddc7cba63977f68fd189
mario38lg23 <mariolopezgas@gmail.com>
2025-05-06 20:33:00 +0200

    el mapa ya se ve

--- src/main/java/com/rodasfiti/controllers/VistaJuego.java
+++ src/main/java/com/rodasfiti/controllers/VistaJuego.java
@@ -1,79 +1,129 @@
 package com.rodasfiti.controllers;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-import javafx.scene.media.Media;
-
-import java.net.URL;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.ColumnConstraints;
 
 import com.rodasfiti.model.Escenario;
 import com.rodasfiti.model.Protagonista;
 import com.rodasfiti.model.Proveedor;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 public class VistaJuego {
 
     @FXML
     private Label vida;
+
     @FXML
     private Label escudo;
+
     @FXML
     private Label ataque;
+
     @FXML
     private Label nivel;
+
     @FXML
     private Label nombrePersonaje;
+
     @FXML
-    private GridPane mapa;
+    private AnchorPane contenedorMapa;
+
+    private GridPane mainGrid;
 
     @FXML
     public void initialize() {
-
         actualizarDatosProtagonista();
+        mainGrid = new GridPane();
+        mainGrid.setPadding(new Insets(10));
+        mainGrid.setHgap(0);
+        mainGrid.setVgap(0);
+        AnchorPane.setTopAnchor(mainGrid, 0.0);
+        AnchorPane.setLeftAnchor(mainGrid, 0.0);
+        AnchorPane.setRightAnchor(mainGrid, 0.0);
+        AnchorPane.setBottomAnchor(mainGrid, 0.0);
 
+        // Cargar mapa desde CSV
+        Escenario escenario = cargarEscenarioDesdeRecursos("/com/rodasfiti/data/mapa.csv");
+        if (escenario != null) {
+            mostrarMapa(escenario);
+        } else {
+            System.err.println("No se pudo cargar el escenario.");
+        }
+
+        contenedorMapa.getChildren().add(mainGrid);
     }
 
-    public void actualizarDatosProtagonista() {
+    void actualizarDatosProtagonista() {
         Protagonista protagonista = Proveedor.getInstance().getProtagonista();
         nombrePersonaje.setText(protagonista.getNombre());
         vida.setText(String.valueOf(protagonista.getVida()));
         ataque.setText(String.valueOf(protagonista.getAtaque()));
         escudo.setText(String.valueOf(protagonista.getDefensa()));
         nivel.setText(String.valueOf(protagonista.getNivel()));
-
     }
 
-    public void mostrarMapa(Escenario escenario, GridPane gridPane) {
+    private Escenario cargarEscenarioDesdeRecursos(String ruta) {
+        try {
+            URL resource = getClass().getResource(ruta);
+            if (resource == null) {
+                System.err.println("No se encontró el archivo: " + ruta);
+                return null;
+            }
+            Path path = Paths.get(resource.toURI());
+            return Escenario.cargarDesdeCSV(path);
+        } catch (Exception e) {
+            System.err.println("Error al cargar el escenario: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void mostrarMapa(Escenario escenario) {
         char[][] mapa = escenario.getMapa();
-        gridPane.getChildren().clear();
+        mainGrid.getChildren().clear();
 
         for (int fila = 0; fila < mapa.length; fila++) {
             for (int col = 0; col < mapa[fila].length; col++) {
                 String rutaImagen;
-
-                if (mapa[fila][col] == 'P') {
-                    rutaImagen = "/com/rodasfiti/images/pared2.png";
-                } else if (mapa[fila][col] == 'S') {
-                    rutaImagen = "/com/rodasfiti/images/suelo2.png";
-                } else {
-                    continue;
+                switch (mapa[fila][col]) {
+                    case 'P':
+                        rutaImagen = "/com/rodasfiti/images/pared2.png";
+                        break;
+                    case 'S':
+                        rutaImagen = "/com/rodasfiti/images/suelo2.png";
+                        break;
+                    default:
+                        rutaImagen = "/com/rodasfiti/images/suelo2.png"; 
                 }
 
-                ImageView imageView = new ImageView(new Image(getClass().getResourceAsStream(rutaImagen)));
-                imageView.setFitWidth(32);
-                imageView.setFitHeight(32);
-                gridPane.add(imageView, col, fila);
+                Image image = cargarImagen(rutaImagen);
+                if (image != null) {
+                    ImageView imageView = new ImageView(image);
+                    imageView.setFitWidth(96);
+                    imageView.setFitHeight(67);
+                    mainGrid.add(imageView, col, fila);
+                }
             }
         }
     }
 
+    private Image cargarImagen(String ruta) {
+        try {
+            return new Image(Objects.requireNonNull(getClass().getResourceAsStream(ruta)));
+        } catch (Exception e) {
+            System.err.println("No se pudo cargar la imagen: " + ruta);
+            return null;
+        }
+    }
 }
