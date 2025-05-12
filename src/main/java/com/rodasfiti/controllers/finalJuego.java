ef489b4990c1b6df6d01062dea894a0554e798ad
Nuel2004 <manurodaspinilla@gmail.com>
2025-05-12 20:58:19 +0200

    nise q es esto ya estoy hasta la polla

--- src/main/java/com/rodasfiti/controllers/finalJuego.java
+++ src/main/java/com/rodasfiti/controllers/finalJuego.java
@@ -12,8 +12,6 @@ import com.rodasfiti.SceneManager;
 
 import javafx.fxml.FXML;
 
-
-
 public class finalJuego {
     @FXML
     private Button salir;
@@ -34,31 +32,21 @@ public class finalJuego {
             }
             SceneManager.getInstance().loadScene(SceneID.MAINVISTA);
         });
-        salir.setOnAction(event->{
+        salir.setOnAction(event -> {
             SceneManager.getInstance().getStage().close();
         });
         cargarMusica();
     }
+
     private void cargarMusica() {
-        try {
-            // Ruta fija (recomendado si sabes el nombre del archivo)
-            String rutaAudio = "/com/rodasfiti/media/Lord_of_the_Rings_Sound_of_The_Shire.mp3";
-
-            URL url = getClass().getResource(rutaAudio);
-
-            if (url == null) {
-                System.err.println("Archivo de audio no encontrado: " + rutaAudio);
-            } else {
-                Media media = new Media(rutaAudio);
-                this.mediaPlayer = new MediaPlayer(media); // Asignamos la instancia de MediaPlayer al campo
-                musica.setMediaPlayer(this.mediaPlayer); // Usamos el mediaPlayer de la clase
-                this.mediaPlayer.setVolume(0.6);
-                this.mediaPlayer.play();
-            }
-        } catch (Exception e) {
-            System.err.println("Error al cargar audio: " + e.getMessage());
+        URL resource = getClass().getResource("/com/rodasfiti/media/final.mp3");
+        if (resource != null) {
+            Media sound = new Media(resource.toExternalForm());
+            MediaPlayer mediaPlayer = new MediaPlayer(sound);
+            mediaPlayer.play();
+        } else {
+            System.err.println("No se pudo cargar el recurso de audio.");
         }
     }
-    
 
 }
